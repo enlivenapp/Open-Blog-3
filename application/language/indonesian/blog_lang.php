3541a5ded62fbcf1f61e4f59ce881f437c27a92c
suhindra <suhindra@hotmail.co.id>
2017-03-16 15:10:33 +0700

    translate pages and blog lang

--- application/language/indonesian/blog_lang.php
+++ application/language/indonesian/blog_lang.php
@@ -1,125 +1,124 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 // headers
-$lang['recent_posts']					= "Recent Posts";
-$lang['older_posts']					= "Older Posts";
-$lang['connect_hdr']					= "Connect";
-$lang['blog_links_hdr']					= "Blog Links";
-$lang['links_hdr']						= "Links";
-$lang['archives_hdr']					= "Archives";
-$lang['categories_hdr']					= "Categories";
-$lang['blog_notices_hdr']				= "Subscribe";
-$lang['blog_notices_unsub_hdr']			= "Unsubscribe";
+$lang['recent_posts']					= "Pos Terbaru";
+$lang['older_posts']					= "Pos Lama";
+$lang['connect_hdr']					= "Hubungan";
+$lang['blog_links_hdr']					= "Tautan Blog";
+$lang['links_hdr']						= "Tautan";
+$lang['archives_hdr']					= "Arsip";
+$lang['categories_hdr']					= "Kategori";
+$lang['blog_notices_hdr']				= "Berlangganan";
+$lang['blog_notices_unsub_hdr']			= "Berhenti Berlangganan";
 				
-$lang['category_hdr']					= "Posts in Category ";
-$lang['archives_for_hdr']				= "Archives for ";
-$lang['blog_notices_help_txt']			= "Get an email when new content is added.";
-$lang['blog_notices_help_unsub_txt']	= "Enter the email address to Unsubscribe.";
+$lang['category_hdr']					= "Pos dalam Kategori ";
+$lang['archives_for_hdr']				= "Arsip untuk ";
+$lang['blog_notices_help_txt']			= "Dapatkan email jika ada konten baru.";
+$lang['blog_notices_help_unsub_txt']	= "Masukan email untuk berhenti berlangganan.";
 
 
 // General Buttons
-$lang['btn_read_more'] 					= "Read more&hellip;";
+$lang['btn_read_more'] 					= "Selenglapnya&hellip;";
 $lang['btn_edit']						= "Edit";
-$lang['btn_remove']						= "Remove";
-$lang['btn_unpub']						= "Un-publish";
-$lang['submit']							= "Submit";
+$lang['btn_remove']						= "Hapus";
+$lang['btn_unpub']						= "Batal terbitkan";
+$lang['submit']							= "Kirim";
 
 
 // Comments
-$lang['comments_disabled']				= "Comments are disabled.";
-$lang['comments_disabled_post']			= "Comments are disabled for this post.";
-$lang['comments']						= "Comments";
-$lang['comment']						= "Comment";
-$lang['comments_title']					= "Comments";
-$lang['comment_submit']					= "Submit Comment";
-$lang['no_comments']					= "No comments yet";
-$lang['btn_approve_comment']			= "Approve Comment"; // Approve modded comment
-$lang['btn_remove_comment']				= "Remove Comment"; // Deletes the comment
-$lang['btn_hide_comment']				= "Hide Comment";  // Hides Comment, provides link to 'view anyway'
-$lang['btn_flag_comment']				= "Flag Comment";  // flag as inappropriate, unhelpful, or SPAM
-$lang['comment_help_text']				= "Note: No HTML";
+$lang['comments_disabled']				= "Komentar dinonaktifkan.";
+$lang['comments_disabled_post']			= "Komentar dinonaktifkan untuk pos ini.";
+$lang['comments']						= "Komentar";
+$lang['comment']						= "Komentar";
+$lang['comments_title']					= "Komentar";
+$lang['comment_submit']					= "Kirim Komentar";
+$lang['no_comments']					= "Tidak ada Komentar";
+$lang['btn_approve_comment']			= "Setujui Komentar"; // Approve modded comment
+$lang['btn_remove_comment']				= "Hapus Komentar"; // Deletes the comment
+$lang['btn_hide_comment']				= "Sembunyikan Komentar";  // Hides Comment, provides link to 'view anyway'
+$lang['btn_flag_comment']				= "Benderai Komentar";  // flag as inappropriate, unhelpful, or SPAM
+$lang['comment_help_text']				= "Catatan: HTML tidak tersedia";
 
 
 
 // Misc
-$lang['add_to']							= "Add to:";
-$lang['posted_in']						= "Posted in";
-$lang['in']								= "in";
-$lang['by'] 							= "by";
+$lang['add_to']							= "Tambahkan ke:";
+$lang['posted_in']						= "Di pos di";
+$lang['in']								= "di";
+$lang['by'] 							= "oleh";
 
 // old pagination?
-$lang['older_entries']					= "&laquo; Older Entries";
-$lang['newer_entries']					= "Newer Entries &raquo;";
+$lang['older_entries']					= "&laquo; Pos Lebih Lama";
+$lang['newer_entries']					= "Pos Lebih Baru &raquo;";
 
 
 
 
 // Single post
-$lang['leave_reply']					= "Leave a Comment";
-$lang['leave_reply_description']		= "Feel free to share your thoughts about this entry.";
+$lang['leave_reply']					= "Tinggalkan Komentar";
+$lang['leave_reply_description']		= "Jangan ragu untuk berbagi pemikiran anda tentang pos ini.";
 
-$lang['responses_to']					= "Responses (%d) to";
-$lang['on']								= "on";
-$lang['at']								= "at";
-$lang['nickname']						= "Name";
+$lang['responses_to']					= "Tanggapan untuk (%d)";
+$lang['on']								= "pada";
+$lang['at']								= "pada";
+$lang['nickname']						= "Nama";
 $lang['email']							= "Email";
 //$lang['website']						= "Website";  // Depreciated
-$lang['confirmation_image']				= "Confirmation image";
-$lang['confirmation_code']				= "Confirmation code";
+$lang['confirmation_image']				= "Gamabar konfirmasi";
+$lang['confirmation_code']				= "Kode konfirmasi";
 
 
 // Tags
-$lang['posts_tagged_with']				= "Posts tagged with";
+$lang['posts_tagged_with']				= "Pos dengan tag";
 
 // Search
-$lang['search_results_for']				= "Search results for";
-$lang['no_results']						= "No results found for this search term.";
-$lang['in']								= "in";
+$lang['search_results_for']				= "Hasil pencarian untuk";
+$lang['no_results']						= "Tak ditemukan hasil apapun unutk terma pencarian ini.";
+$lang['in']								= "di";
 
 // Errors
-$lang['error']							= "Error";
-$lang['not_found']						= "Not found.";
-$lang['no_posts_for_this_tag']			= "There are no posts for this tag.";
-$lang['no_posts_for_this_date']			= "There are no posts for this date.";
-$lang['no_posts_found']					= "No posts found";
-$lang['blog_no_posts']					= "This blog has no posts.";
-$lang['invalid_confirmation_code']		= "The Confirmation code you have entered is invalid.";
+$lang['error']							= "Galat";
+$lang['not_found']						= "Tidak ditemukan.";
+$lang['no_posts_for_this_tag']			= "Tak ada pos untuk tag ini.";
+$lang['no_posts_for_this_date']			= "Tak ada pos untuk tanggal ini.";
+$lang['no_posts_found']					= "Pos tidak ditemukan";
+$lang['blog_no_posts']					= "Tidak ada pos di blog ini.";
+$lang['invalid_confirmation_code']		= "Kode konfirmasi yang anda masukan tidak sah.";
 $lang['error_404_heading']				= "Ooops!";
-$lang['error_404_message']				= "There was an error. Please check the URL and try again.";
+$lang['error_404_message']				= "Terjadi kesalahan. Silakan periksa URL dan coba lagi.";
 $lang['recaptcha']						= "reCAPTCHA";
 
 // Success
-$lang['add_comment_success']			= "Comment Added Successfully.";
-$lang['add_comment_success_modded']		= "Comment Added Successfully. Your comment will appear once approved by an administrator.";
+$lang['add_comment_success']			= "Komentar Berhasil Ditambahkan.";
+$lang['add_comment_success_modded']		= "Komentar Berhasil Ditambahkan. Komentar akan tampil bila telah disetujui oleh administrator.";
 
 
 // Emails
-$lang['email_new_comment_sbj']			= "New Comment";
-$lang['email_new_comment_msg']			= "You have a new comment.  You can manage it in the Control Panel <br><br>The new Comment:<br><br>";
+$lang['email_new_comment_sbj']			= "Komentar Baru";
+$lang['email_new_comment_msg']			= "Anda mempunyai komentar baru.  Anda bisa mengelolanya di Panel Kontrol <br><br>Komentar Baru:<br><br>";
 
 
 // notices
-$lang['notices_enter_email_address']	= "Enter Email Address";
-$lang['notices_get_notices']			= "Get Notified!";
-$lang['notices_unsub_btn']				= "Unsubscribe";
-$lang['notices_no_post_data']			= "Error. Please try again.";
-$lang['email_address']					= "Email Address";
-$lang['notify_new_notification']		= 'Nofifications Requested';
-$lang['notices_email_verify_msg']		= "Someone requested this email address to receive notifications when we post new content on our website.  If this is correct, click or follow the link below to verify the email, if you did not, you can ignore this email and we will not send you any notices. <br><br>Verification Link: ";
-$lang['notices_verify_failed']			= 'It seems the verification failed for some reason.  Please check your email and try again.';
-$lang['notices_verify_success']			= "Thank you for verifying your email address.  We'll send you a confirmation email too!";
-$lang['notify_success']					= 'Successful Verification';
-$lang['notices_success_verifed_msg']	= 'Thank you for verifying your email address.  We will begin sending you a notice when we add content to our blog.';
-$lang['notices_add_success']			= 'Thank you for your interest in our content!  Please check your email for further instructions to verify your email address.';
-$lang['notices_email_exists']			= 'Looks like we already have this address.';
-$lang['notices_email_not_exists']		= "Looks like we don't have that address.  Try again?";
-$lang['notify_unsub_success']			= "Successfully Unsubscribed to new content. We're sorry to see you go";
-$lang['notify_unsub_sbj']				= 'Unsubscribed';
-$lang['notices_success_unsub_msg']		= 'This email is to confirm successfully unsubscribing from new content.  <b>We will miss you!</b><br><br>This will be the last email you receive unless you re-subscribe.';
+$lang['notices_enter_email_address']	= "Masukan Alamat Email";
+$lang['notices_get_notices']			= "Dapatkan Pemberitahuan!";
+$lang['notices_unsub_btn']				= "Berhenti berlangganan";
+$lang['notices_no_post_data']			= "Galat. Silakan coba lagi.";
+$lang['email_address']					= "Alamat Email";
+$lang['notify_new_notification']		= "Pemberitahuan telah diajukan";
+$lang['notices_email_verify_msg']		= "Seseorang meminta alamat email ini untuk menerima pemberitahuan ketika kita posting konten baru di website kami. Jika ini benar, klik atau mengikuti link di bawah ini untuk memverifikasi email, jika Anda tidak, Anda dapat mengabaikan email ini dan kami tidak akan mengirimkan pemberitahuan apapun. <br><br>Tautan Verivikasi: ";
+$lang['notices_verify_failed']			= 'Tampaknya verifikasi gagal karena beberapa alasan. Silahkan cek email Anda dan coba lagi.';
+$lang['notices_verify_success']			= "Terima kasih telah memverifikasi alamat email Anda. Kami akan mengirimkan email konfirmasi juga!";
+$lang['notify_success']					= 'Verivikasi Berhasil';
+$lang['notices_success_verifed_msg']	= 'Terima kasih telah memverifikasi alamat email Anda. Kami akan mulai mengirimkan pemberitahuan ketika kita menambahkan konten ke blog kita.';
+$lang['notices_add_success']			= 'Terima kasih atas minat Anda pada konten kami! Silahkan cek email Anda untuk petunjuk lebih lanjut untuk memverifikasi alamat email Anda.';
+$lang['notices_email_exists']			= 'Sepertinya kita sudah memiliki alamat ini.';
+$lang['notices_email_not_exists']		= "Sepertinya kita tidak memiliki alamat tersebut. Coba lagi?";
+$lang['notify_unsub_success']			= "Berhasil berhenti berlangganan ke konten baru. Kami sedih melihat Anda pergi";
+$lang['notify_unsub_sbj']				= 'Berhenti Berlangganan';
+$lang['notices_success_unsub_msg']		= 'Email ini untuk mengkonfirmasi berhasil berhenti berlangganan dari konten baru.  <b>Kami akan merindukanmu!</b><br><br>Ini akan menjadi email terakhir yang Anda terima kecuali Anda berlangganan kembali.';
 
 //$lang['']	= '';
 
 // public language
-$lang['language_changed_successfully']	= 'Language Changed Successfully';
-$lang['language_not_available']			= 'We don not seem to have that language available.';
-
+$lang['language_changed_successfully']	= 'Bahasa Berhasil Diubah';
+$lang['language_not_available']			= 'Kami tampaknya tidak memiliki bahasa yang tersedia.';
